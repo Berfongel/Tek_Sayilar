4cc0426b502d0382db0a2417067b92d45181129c
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:09:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi;
+//1 den 100 e kadar olan 
+for (sayi=1;sayi<=100;sayi++) 
+{
+    //2 ile tam bölünmüyorsa sayı tek sayıdır.
+    if (sayi % 2 != 0)
+    {
+        Console.WriteLine("Tek sayılar : " + sayi);
+        sayi++;
+    }
+}
+Console.ReadKey();
