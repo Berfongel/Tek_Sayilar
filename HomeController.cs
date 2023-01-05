3476a72e993fbfa3605dad10242b6d1d2e5da35e
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-01-05 13:45:52 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TekSayilarWebApplication.Models;
+
+namespace TekSayilarWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TekSayilar tekSayilar)
+        {
+            var message = string.Empty;
+           ViewBag.Message = message;
+
+            return View("tekSayilar");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TekSayilar.cs
@@ -0,0 +1,7 @@
+﻿namespace TekSayilarWebApplication.Models
+{
+    public class TekSayilar
+    {
+        public int TekSayi { get; set; }
+    }
+}
